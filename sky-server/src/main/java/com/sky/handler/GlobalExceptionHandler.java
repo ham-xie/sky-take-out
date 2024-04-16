b52ded2eaa357eb5963bfe6a70a607910f9bef15
ham-xie <x-qx@foxmail.com>
2024-04-16 17:51:48 +0800

    新增员工业务代码

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -24,4 +27,23 @@ public class GlobalExceptionHandler {
         return Result.error(ex.getMessage());
     }
 
+    /**
+     * 处理用户名重复的异常
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        String msg = ex.getMessage();
+        if (msg.contains("Duplicate entry")){
+            //说明是这个异常
+            String[] spilt = msg.split(" ");
+            String username = spilt[2];
+            String errorMsg = username + MessageConstant.ALREADY_EXISTS;
+            return Result.error(errorMsg);
+        }else {
+            String errorMsg = MessageConstant.UNKNOWN_ERROR;
+            return Result.error(errorMsg);
+        }
+    }
 }
